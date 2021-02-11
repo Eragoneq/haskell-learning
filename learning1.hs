a12ebde8ae217d27c59141f1b02c5a1b9bc3f656
Eragoneq <eragon.skill@gmail.com>
2021-02-12 00:28:55 +0100

    Finished part 1 of homework

--- learning1.hs
+++ learning1.hs
@@ -1,3 +1,5 @@
+-- Exercise 1
+
 toDigits :: Integer -> [Integer]
 toRevDigits :: Integer -> [Integer]
 
@@ -6,7 +8,29 @@ toDigits num
     | num < 10 = [num]
     | otherwise = toDigits (num `div` 10) ++ [num `mod` 10]
 
+-- This could just be the upper algorithm 
 toRevDigits num
     | num <= 0 = []
     | num < 10 = [num]
     | otherwise = (num `mod` 10) : toRevDigits (num `div` 10)
+
+-- Exercise 2
+
+-- Kinda weird solution, but it could be improved after I learn a bit more
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther (x:y:xs)
+    | even (length (x:y:xs)) = (2*x) : y : doubleEveryOther xs
+    | otherwise = x : (2*y) : doubleEveryOther xs
+doubleEveryOther (x:xs) = [x]
+doubleEveryOther _ = []
+
+-- Exercise 3
+
+sumDigits :: [Integer] -> Integer
+sumDigits list = sum (map (sum . toDigits) list)
+
+-- Exercise 4
+-- This chain of functions can probably be done in a better way, but I didn't read enough yet
+
+validate :: Integer -> Bool
+validate num = sumDigits (doubleEveryOther (toDigits num)) `mod` 10 == 0
