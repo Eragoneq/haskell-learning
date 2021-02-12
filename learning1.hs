248584e4a6f8b43dd65a053ed57bba15657234be
Eragoneq <eragon.skill@gmail.com>
2021-02-12 12:15:01 +0100

    Added part 2 of homework (Towers of Hanoi)

--- learning1.hs
+++ learning1.hs
@@ -34,3 +34,12 @@ sumDigits list = sum (map (sum . toDigits) list)
 
 validate :: Integer -> Bool
 validate num = sumDigits (doubleEveryOther (toDigits num)) `mod` 10 == 0
+
+-- PART 2
+
+type Peg = String
+type Move = (Peg, Peg)
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi n a b c = hanoi (n-1) a c b ++ [(a,b)] ++ hanoi (n-1) c b a
+
